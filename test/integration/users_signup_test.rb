b5ac5d15f3b85981e5221e29faddb84831726186
hinnahinna <m.serizawa2064@gmail.com>
2020-05-10 01:42:29 +0900

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "invalid signup information" do
+    get signup_path
+    #User数が変わらないかの確認のカウント
+    assert_no_difference 'User.count' do
+      post users_path, params:{user:{
+        name: "",
+        email: "user@invalid",
+        password: "foo",
+        password_confirmation: "bar"
+      }}
+    end
+    assert_template 'users/new'
+    # assert_select 'div#<CSS id for error explanation>'
+    # assert_select 'div.<CSS class for field with error>'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    #User数が変わらないかの確認のカウント 実行前後を比較
+    assert_difference 'User.count' do
+      post users_path, params:{user:{
+        name: "Example User",
+        email: "user@example.com",
+        password: "password",
+        password_confirmation: "password"
+      }}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
+end
